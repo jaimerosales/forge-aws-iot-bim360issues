ce272b10f5853ae3f9a611f8c199887cc273ef5d
Jaime Rosales <jaime.rosales.duque@autodesk.com>
2019-04-09 17:46:50 -0400

    initial authentication setup

---
+++ public/js/ForgeTree.js
@@ -0,0 +1,68 @@
+﻿/////////////////////////////////////////////////////////////////////
+// Copyright (c) Autodesk, Inc. All rights reserved
+// Written by Forge Partner Development
+//
+// Permission to use, copy, modify, and distribute this software in
+// object code form for any purpose and without fee is hereby granted,
+// provided that the above copyright notice appears in all copies and
+// that both that copyright notice and the limited warranty and
+// restricted rights notice below appear in all supporting
+// documentation.
+//
+// AUTODESK PROVIDES THIS PROGRAM "AS IS" AND WITH ALL FAULTS.
+// AUTODESK SPECIFICALLY DISCLAIMS ANY IMPLIED WARRANTY OF
+// MERCHANTABILITY OR FITNESS FOR A PARTICULAR USE.  AUTODESK, INC.
+// DOES NOT WARRANT THAT THE OPERATION OF THE PROGRAM WILL BE
+// UNINTERRUPTED OR ERROR FREE.
+/////////////////////////////////////////////////////////////////////
+
+$(document).ready(function () {
+  // first, check if current visitor is signed in
+  jQuery.ajax({
+    url: '/api/forge/oauth/token',
+    success: function (res) {
+      // yes, it is signed in...
+      $('#signOut').show();
+      $('#refreshHubs').show();
+      $('#userHubs').hide();
+
+      // prepare sign out
+      $('#signOut').click(function () {
+        $('#hiddenFrame').on('load', function (event) {
+          location.href = '/api/forge/oauth/signout';
+        });
+        $('#hiddenFrame').attr('src', 'https://accounts.autodesk.com/Authentication/LogOut');
+      })
+
+      showUser();
+    }
+  });
+
+  $('#autodeskSigninButton').click(function () {
+    jQuery.ajax({
+      url: '/api/forge/oauth/url',
+      success: function (url) {
+        location.href = url;
+      }
+    });
+  })
+});
+
+function showUser() {
+  jQuery.ajax({
+    url: '/api/forge/user/profile',
+    success: function (profile) {
+      var img = '<img src="' + profile.picture + '" height="30px">';
+      $('#userInfo').html(img + profile.name);
+    }
+  });
+}
+
+function getForgeToken(callback) {
+  jQuery.ajax({
+    url: '/api/forge/oauth/token',
+    success: function (res) {
+      callback(res.access_token, res.expires_in)
+    }
+  });
+}
